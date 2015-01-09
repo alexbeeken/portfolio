c76813fd17db5005fa1744e839fb76fe6f738a66
Alex Beeken <aeb242@gmail.com>
2015-01-09 07:35:34 +0000

    Created Gemfile, got app.rb working to link to the right files

---
+++ app.rb
@@ -0,0 +1,96 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/anagram')
+require('./lib/clock')
+require('./lib/coin_combinations')
+require('./lib/find_and_replace')
+require('./lib/palindrome')
+require('./lib/ping_pong')
+require('./lib/queen_attack')
+require('./lib/rochambeau')
+
+configure :development do
+set :bind, '0.0.0.0'
+set :port, 3000 # Not really needed, but works well with the "Preview" menu option
+end
+
+
+get ('/') do
+  erb(:root)
+end
+
+get('/rochambeau_1p') do
+  erb(:rochambeau_1p)
+end
+
+get('/rochambeau_2p') do
+  erb(:rochambeau_2p)
+end
+
+get('/rochambeau_result_1p') do
+  @roch_result = (params.fetch('object').to_i).rochambeau(rand(2))
+  erb(:rochambeau_result)
+end
+  
+get('/rochambeau_result_2p') do
+  @roch_result = (params.fetch('object').to_i).rochambeau((params.fetch('object2').to_i))
+  erb(:rochambeau_result)
+end
+
+get('/fnr_form') do
+  erb(:fnr_form)
+end
+
+get('/fnr_out') do
+  @changed_string = params.fetch('text').fnr(params.fetch('find'), params.fetch('replacement'))
+  erb(:fnr_out)
+end
+
+get('/palindrome_form') do
+  erb(:palindrome_form)
+end
+
+get('/palindrome_result') do
+  @input = params.fetch('palindrome')
+  @result = params.fetch('palindrome').palindrome()
+  erb(:palindrome_result)
+end
+
+get('/anagram_form') do
+  erb(:anagram_form)
+end
+
+get('/anagram_out') do
+  @word = params.fetch('anagram').anagram(params.fetch('word_list'))
+  erb(:anagram_out)
+end
+
+get('/coins') do
+  erb(:coins_form)
+end
+
+get('/coins_out') do
+  @coins_output = params.fetch('coins').to_i.coin_combinations()
+  erb(:coins_out)
+end
+
+get('/ping-pong') do
+  @pp_out = params.fetch('pp_input').ping_pong()
+  erb(:ping_pong_show)
+end
+
+get('/queen_attack') do
+@q_result = ([(params.fetch('queen_x')).to_i, (params.fetch('queen_y')).to_i]).queen_attack([(params.fetch('attacker_x')).to_i,  (params.fetch('attacker_y')).to_i])
+erb(:queen_attack_show)
+end
+
+get('/clock') do
+  @c_hours = params.fetch('c_hours')
+  @c_minutes = params.fetch('c_minutes')
+  @c_output = [(params.fetch('c_hours')).to_i,(params.fetch('c_minutes')).to_i].clock()
+  @c_output_long = (360.00 - @c_output)
+  erb(:clock_show)
+end 
+  
+
+  
--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,23 +1,25 @@
 class Array
   define_method(:queen_attack) do |attacker|
 
+    result = ""
+    
     if ((attacker[0] == (self[0])) && (attacker[1] == (self[1])))
-      false
+      result = "the queen cannot attack herself!"
 
     elsif (attacker[1].eql?(self[1]))
-      true
+      result = "the queen is under attack from the north and south!"
 
     elsif (attacker[0].eql?(self[0]))
-      true
+      result = "the queen is under attack from her sides!"
 
     elsif ((attacker[0].-(self[0])).abs.eql?(attacker[1].-(self[1]).abs))
-      true
+      result = "the queen is under attack under a diagonol attack"
 
     else
-      false
+      result = "the queen is safe."
 
     end
-
+    result
   end
 
 end
